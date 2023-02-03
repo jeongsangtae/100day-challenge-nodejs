d47babc4db309ab17b1fb285e8e6e83d23c2256f
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-03 20:57:11 +0900

    End of Day 51 (ExpressJS - NPM uuid)

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const express = require("express");
 
+const uuid = require("uuid");
+
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -44,6 +46,7 @@ app.get("/recommend", function (req, res) {
 
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, "data", "restaurants.json");
 
   const fileData = fs.readFileSync(filePath);
