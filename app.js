092db5bb959600d321c371edf96e19dc0eb23a99
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-04 22:00:56 +0900

    NodeJS - code import & export

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// 내장된 패키지
 const fs = require("fs");
-
 const path = require("path");
 
+// 타사 패키지
 const express = require("express");
-
 const uuid = require("uuid");
+
+const resData = require("./util/restaurant-data");
 
 const app = express();
 
@@ -23,10 +25,7 @@ app.get("/", function (req, res) {
 app.get("/restaurants", function (req, res) {
   // const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
   // res.sendFile(htmlFilePath);
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
   res.render("restaurants", {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
@@ -35,10 +34,7 @@ app.get("/restaurants", function (req, res) {
 
 app.get("/restaurants/:id", function (req, res) {
   const restaurantId = req.params.id;
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
 
   for (const restaurant of storedRestaurants) {
     if (restaurant.id === restaurantId) {
@@ -58,11 +54,11 @@ app.get("/recommend", function (req, res) {
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
   restaurant.id = uuid.v4();
-  const restaurantsData = getStoredRestaurants();
+  const restaurantsData = resData.getStoredRestaurants();
 
   restaurantsData.push(restaurant);
 
-  storeRestaurants(restaurantsData);
+  resData.storeRestaurants(restaurantsData);
 
   res.redirect("/confirm");
 });
