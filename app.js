45605d8f7005676f92dc4a28077f91389f22d705
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-04 23:18:10 +0900

    ExpressJS - express router using split route configuration

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // 내장된 패키지
-const fs = require("fs");
 const path = require("path");
 
 // 타사 패키지
 const express = require("express");
-const uuid = require("uuid");
 
-const resData = require("./util/restaurant-data");
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurants");
 
 const app = express();
 
@@ -16,64 +15,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-  // const htmlFilePath = path.join(__dirname, "views", "index.html");
-  // res.sendFile(htmlFilePath);
-  res.render("index");
-});
-
-app.get("/restaurants", function (req, res) {
-  // const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  // res.sendFile(htmlFilePath);
-  const storedRestaurants = resData.getStoredRestaurants();
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get("/restaurants/:id", function (req, res) {
-  const restaurantId = req.params.id;
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res.render("restaurant-detail", { restaurant: restaurant });
-    }
-  }
-
-  res.status(404).render("404");
-});
-
-app.get("/recommend", function (req, res) {
-  // const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-  // res.sendFile(htmlFilePath);
-  res.render("recommend");
-});
-
-app.post("/recommend", function (req, res) {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const restaurantsData = resData.getStoredRestaurants();
-
-  restaurantsData.push(restaurant);
-
-  resData.storeRestaurants(restaurantsData);
-
-  res.redirect("/confirm");
-});
-
-app.get("/confirm", function (req, res) {
-  // const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  // res.sendFile(htmlFilePath);
-  res.render("confirm");
-});
-
-app.get("/about", function (req, res) {
-  // const htmlFilePath = path.join(__dirname, "views", "about.html");
-  // res.sendFile(htmlFilePath);
-  res.render("about");
-});
+app.use("/", defaultRoutes);
+app.use("/", restaurantRoutes);
 
 app.use(function (req, res) {
   res.status(404).render("404");
---
+++ routes/restaurants.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const uuid = require("uuid");
+
+const resData = require("../util/restaurant-data");
+
+const router = express.Router();
+
+router.get("/restaurants", function (req, res) {
+  // const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  // res.sendFile(htmlFilePath);
+  const storedRestaurants = resData.getStoredRestaurants();
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+router.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  const storedRestaurants = resData.getStoredRestaurants();
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant-detail", { restaurant: restaurant });
+    }
+  }
+
+  res.status(404).render("404");
+});
+
+router.get("/recommend", function (req, res) {
+  // const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  // res.sendFile(htmlFilePath);
+  res.render("recommend");
+});
+
+router.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  restaurant.id = uuid.v4();
+  const restaurantsData = resData.getStoredRestaurants();
+
+  restaurantsData.push(restaurant);
+
+  resData.storeRestaurants(restaurantsData);
+
+  res.redirect("/confirm");
+});
+
+router.get("/confirm", function (req, res) {
+  // const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  // res.sendFile(htmlFilePath);
+  res.render("confirm");
+});
+
+module.exports = router;
